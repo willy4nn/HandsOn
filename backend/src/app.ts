a1b5535916776d6eb52032835999645e9af50df5
Willyan <willy4nnp@gmail.com>
2025-03-13 18:03:47 -0300

    backend/feature: Implement user authentication with LoginUseCase and authMiddleware
    - Created LoginUserUseCase for handling user login and token generation.
    - Developed LoginUserController for processing login requests and setting tokens in cookies.
    - Added authMiddleware to validate JWT token and user authentication for protected routes.
    - Updated routes to include login functionality.
    - Integrated cookie parsing middleware in the app for token handling.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { router } from "./routes";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json()); // Enable JSON request body parsing
+app.use(cookieParser()); // Middleware to parse cookies from incoming requests
 app.use(router); // Register application routes
 app.use(errorMiddleware); // Middleware to handle errors
 
