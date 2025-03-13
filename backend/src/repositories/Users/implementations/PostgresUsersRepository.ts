de66d9938f3b7f4e06fd12a4bc37efcdc5cf0e29
Willyan <willy4nnp@gmail.com>
2025-03-13 18:16:10 -0300

    backend/feature: Implement findById and update methods in PostgresUsersRepository
    - Added findById method to find a user by their ID in PostgresUsersRepository.
    - Added update method to update a user's details in the repository.
    - Integrated CustomError for error handling in both methods.

--- backend/src/repositories/Users/implementations/PostgresUsersRepository.ts
+++ backend/src/repositories/Users/implementations/PostgresUsersRepository.ts
@@ -29,6 +29,31 @@ export class PostgresUsersRepository implements IUsersRepository {
 		}
 	}
 
+	// Method to update user
+	async update(user: User): Promise<void> {
+		const client = await pool.connect();
+		try {
+			await client.query(
+				"UPDATE users SET name = $1, role = $2, email = $3, password = $4, updated_at = $5 WHERE id = $6",
+				[
+					user.name,
+					user.role,
+					user.email,
+					user.password,
+					user.updatedAt,
+					user.id,
+				]
+			);
+		} catch (error) {
+			throw new CustomError(
+				ErrorCatalog.ERROR.USER.REPOSITORY.USER_UPDATE_FAILED,
+				error.message
+			);
+		} finally {
+			client.release(); // Release the client
+		}
+	}
+
 	// Method to find user by email
 	async findByEmail(email: string): Promise<User> {
 		const client = await pool.connect();
@@ -59,6 +84,43 @@ export class PostgresUsersRepository implements IUsersRepository {
 		} catch (error) {
 			throw new CustomError(
 				ErrorCatalog.ERROR.USER.REPOSITORY.QUERY_FAILED
+			);
+		} finally {
+			client.release(); // Release the client
+		}
+	}
+
+	// Method to find user by id
+	async findById(id: string): Promise<User> {
+		const client = await pool.connect();
+		try {
+			const res = await client.query(
+				"SELECT * FROM users WHERE id = $1",
+				[id]
+			);
+			if (res.rows.length > 0) {
+				const userRow = res.rows[0];
+
+				// Pass props and id to the User constructor
+				const user = new User(
+					{
+						name: userRow.name,
+						email: userRow.email,
+						password: userRow.password,
+						role: userRow.role,
+					},
+					userRow.id,
+					new Date(userRow.created_at),
+					new Date(userRow.updated_at)
+				);
+
+				return user;
+			}
+			return null;
+		} catch (error) {
+			throw new CustomError(
+				ErrorCatalog.ERROR.USER.REPOSITORY.USER_NOT_FOUND,
+				error.message
 			);
 		} finally {
 			client.release(); // Release the client
