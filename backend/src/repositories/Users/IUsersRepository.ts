7f962f2ce6a37aec3cbc87a55e77d4fe33507332
Willyan <willy4nnp@gmail.com>
2025-03-13 18:16:10 -0300

    backend/feature: Implement findById and update methods in PostgresUsersRepository
    - Added findById method to find a user by their ID in PostgresUsersRepository.
    - Added update method to update a user's details in the repository.
    - Integrated CustomError for error handling in both methods.

--- backend/src/repositories/Users/IUsersRepository.ts
+++ backend/src/repositories/Users/IUsersRepository.ts
@@ -4,6 +4,12 @@ export interface IUsersRepository {
 	// Method to save a new user
 	save(user: User): Promise<void>;
 
+	// Method to update a user
+	update(user: User): Promise<void>;
+
 	// Method to find a user by email
 	findByEmail(email: string): Promise<User>;
+
+	// Method to find a user by id
+	findById(id: string): Promise<User>;
 }
