a0eb7207cf9d61d5a4312f078c98d052e434fe95
Willyan <willy4nnp@gmail.com>
2025-03-13 16:56:52 -0300

    backend/feature: Implement user creation use case and DTOs
    - Created ICreateUserRequestDTO and ICreateUserResponseDTO for user creation data structure.
    - Implemented CreateUserUseCase to handle user creation logic.
    - Added email validation and password hashing.
    - Integrated user repository to check for existing users and save new ones.

---
+++ backend/src/useCases/User/CreateUser/CreateUserDTO.ts
@@ -0,0 +1,16 @@
+// Fields required to create a user
+export interface ICreateUserRequestDTO {
+	name: string;
+	email: string;
+	password: string;
+}
+
+// Fields returned in the response after creating a user
+export interface ICreateUserResponseDTO {
+	id: string;
+	name: string;
+	email: string;
+	role: string;
+	created_at: string;
+	updated_at: string;
+}
