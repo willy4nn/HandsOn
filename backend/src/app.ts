94d8a5a9edfdee53e4788f8583ad00056c6fa779
Willyan <willy4nnp@gmail.com>
2025-03-13 17:30:29 -0300

    backend/feature: Implement errorMiddleware for centralized error handling
    - Created errorMiddleware to handle and format error responses.
    - Integrated ApiResponse and CustomError for consistent error structure.
    - Added errorMiddleware to app for centralized error management.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { router } from "./routes";
+import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 const app = express();
 app.use(express.json()); // Enable JSON request body parsing
 app.use(router); // Register application routes
+app.use(errorMiddleware); // Middleware to handle errors
 
 export { app };
