905caada6099c1c1b643e1cbbdbc28369ee10bf7
Willyan <willy4nnp@gmail.com>
2025-03-13 18:05:54 -0300

    backend/feature: Implement LogoutUserController for user logout functionality
    - Created LogoutUserController to handle user logout and clear authentication cookies.
    - Added /logout route to invalidate user sessions.
    - Integrated authMiddleware to ensure only authenticated users can log out.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { createUserController } from "./useCases/User/CreateUser";
 import { loginUserController } from "./useCases/User/LoginUser";
+import { logoutUserController } from "./useCases/User/LogoutUser";
+import { authMiddleware } from "./middlewares/authMiddleware";
 
 const router = Router();
 
@@ -12,6 +14,11 @@ router.post("/users", (request, response, next) => {
 // POST route to log in a user
 router.post("/login", (request, response, next) => {
 	return loginUserController.handle(request, response, next);
+});
+
+// POST route to log out a user
+router.post("/logout", authMiddleware, (request, response, next) => {
+	return logoutUserController.handle(request, response, next);
 });
 
 export { router };
