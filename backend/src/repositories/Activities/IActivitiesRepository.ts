61f3fe4b9e75add4f3d8b736464f6b0362c627a6
Willyan <willy4nnp@gmail.com>
2025-03-17 00:06:31 -0300

    backend/feature: Add update method to IActivitiesRepository and PostgresActivitiesRepository
    - Added update method in IActivitiesRepository to update an activity.
    - Implemented update method in PostgresActivitiesRepository to update activity details in the database.
    - Added SQL query to update activity fields like title, date, location, description, etc.

--- backend/src/repositories/Activities/IActivitiesRepository.ts
+++ backend/src/repositories/Activities/IActivitiesRepository.ts
@@ -1,6 +1,9 @@
 import { Activity } from "../../entities/Activity/Activity";
 
 export interface IActivitiesRepository {
-	// Method to save a new activitie
+	// Method to save a new activity
 	save(activity: Activity): Promise<void>;
+
+	// Method to update a activity
+	update(activity: Activity): Promise<void>;
 }
