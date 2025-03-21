1a7a13652e12c3515b41a11e90578f334280547b
Willyan <willy4nnp@gmail.com>
2025-03-20 22:57:13 -0300

    backend/feature: Add delete and findById methods to registrations repository
    - Adds the `delete` method to remove a registration by its ID.
    - Adds the `findById` method to retrieve a registration by its ID.
    - Implements error handling for when the registration is not found.
    - Updates error messages to reflect failures in deleting and finding the registration.

--- backend/src/repositories/Registration/IRegistrationsRepository.ts
+++ backend/src/repositories/Registration/IRegistrationsRepository.ts
@@ -3,4 +3,10 @@ import { Registration } from "../../entities/Registration/Registration";
 export interface IRegistrationsRepository {
 	// Method to save a new registration
 	save(registration: Registration): Promise<void>;
+
+	// Method to delete a registration by its ID
+	delete(id: string): Promise<void>;
+
+	// Method to find a registration by its ID
+	findById(id: string): Promise<Registration>;
 }
--- backend/src/repositories/Registration/implementation/PostgresRegistrationsRepository.ts
+++ backend/src/repositories/Registration/implementation/PostgresRegistrationsRepository.ts
@@ -29,4 +29,66 @@ export class PostgresRegistrationsRepository
 			client.release();
 		}
 	}
+
+	// Method to delete a registration by ID
+	async delete(id: string): Promise<void> {
+		const client = await pool.connect();
+		try {
+			// Delete the registration
+			const result = await client.query(
+				"DELETE FROM registrations WHERE id = $1",
+				[id]
+			);
+
+			// If no rows are affected, throw an error
+			if (result.rowCount === 0) {
+				throw new CustomError(
+					ErrorCatalog.ERROR.REGISTRATION.REPOSITORY.REGISTRATION_NOT_FOUND
+				);
+			}
+		} catch (error) {
+			throw new CustomError(
+				ErrorCatalog.ERROR.REGISTRATION.REPOSITORY.REGISTRATION_DELETE_FAILED,
+				error.message
+			);
+		} finally {
+			client.release();
+		}
+	}
+
+	// Method to find a registration by ID
+	async findById(id: string): Promise<Registration> {
+		const client = await pool.connect();
+		try {
+			const result = await client.query(
+				"SELECT * FROM registrations WHERE id = $1",
+				[id]
+			);
+			if (result.rowCount === 0) {
+				throw new CustomError(
+					ErrorCatalog.ERROR.REGISTRATION.REPOSITORY.REGISTRATION_NOT_FOUND
+				);
+			}
+			const {
+				id: regId,
+				user_id,
+				activity_id,
+				created_at,
+			} = result.rows[0];
+
+			return new Registration({
+				id: regId,
+				userId: user_id,
+				activityId: activity_id,
+				createdAt: created_at,
+			});
+		} catch (error) {
+			throw new CustomError(
+				ErrorCatalog.ERROR.REGISTRATION.REPOSITORY.REGISTRATION_NOT_FOUND,
+				error.message
+			);
+		} finally {
+			client.release();
+		}
+	}
 }
