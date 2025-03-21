d16ed246b1d9b3b34ff8ad576a5c668b020a52fc
Willyan <willy4nnp@gmail.com>
2025-03-20 23:29:57 -0300

    backend/feature: Implement delete registration use case with validation
    - Adds `IDeleteRegistrationRequestDTO` to define the required fields for deleting a registration.
    - Implements `deleteRegistrationValidator` to validate the registration ID before deletion.
    - Creates `DeleteRegistrationUseCase` to handle the registration deletion process.
      - Validates input data.
      - Ensures the registration and related activity exist before deletion.
      - Deletes the registration and updates the activity's current participant count.
    - Improves error handling for missing registrations and activities.

---
+++ backend/src/useCases/Registration/DeleteRegistration/DeleteRegistrationUseCase.ts
@@ -0,0 +1,48 @@
+import { Registration } from "../../../entities/Registration/Registration";
+import { CustomError, ErrorCatalog } from "../../../errors/CustomError";
+import { IDeleteRegistrationRequestDTO } from "./DeleteRegistrationDTO";
+import { IRegistrationsRepository } from "../../../repositories/Registration/IRegistrationsRepository";
+import { IActivitiesRepository } from "../../../repositories/Activities/IActivitiesRepository";
+import { deleteRegistrationValidator } from "./DeleteRegistrationValidator";
+
+export class DeleteRegistrationUseCase {
+	constructor(
+		private registrationsRepository: IRegistrationsRepository,
+		private activitiesRepository: IActivitiesRepository
+	) {}
+
+	async execute(data: IDeleteRegistrationRequestDTO) {
+		// Validate the registration data
+		deleteRegistrationValidator(data);
+		const { registrationId } = data;
+
+		// Check if the registration exists
+		const foundRegistration = await this.registrationsRepository.findById(
+			registrationId
+		);
+		if (!foundRegistration) {
+			throw new CustomError(
+				ErrorCatalog.ERROR.REGISTRATION.SERVICE.REGISTRATION_NOT_FOUND
+			);
+		}
+
+		// Check if the related activity exists
+		const foundActivity = await this.activitiesRepository.findById(
+			foundRegistration.activityId
+		);
+		if (!foundActivity) {
+			throw new CustomError(
+				ErrorCatalog.ERROR.ACTIVITY.SERVICE.ACTIVITY_NOT_FOUND
+			);
+		}
+
+		// Delete the registration
+		await this.registrationsRepository.delete(registrationId);
+
+		// Update the number of current participants in the activity
+		await this.activitiesRepository.update({
+			...foundActivity,
+			currentParticipants: foundActivity.currentParticipants - 1,
+		});
+	}
+}
